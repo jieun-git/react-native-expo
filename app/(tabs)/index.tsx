7321226e1faf48edfe61a0bd58759e5a27363730
jieun <forworkje21@gmail.com>
2024-08-06 21:12:17 +0900

    Feat: WebView 를 이용하여 네이버 웹 사이트 로드

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,17 +5,23 @@ import {
   Platform,
   StatusBar,
 } from "react-native";
+import WebView from "react-native-webview";
 
 const styles = StyleSheet.create({
   safearea: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
   },
 });
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.safearea}>
-      <Text>Home screen</Text>
+      <WebView
+        source={{ uri: "https://m.naver.com" }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      />
     </SafeAreaView>
   );
 };
